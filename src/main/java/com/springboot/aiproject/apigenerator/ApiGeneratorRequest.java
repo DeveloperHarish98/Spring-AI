ca473cbd384e670335bff040d8e5908289a623d0
Harish_Dubey98 <131913836+DeveloperHarish98@users.noreply.github.com>
2026-05-15 12:52:35 +0530

    Delete src/main/java/com/springboot/aiproject/apigenerator directory

--- src/main/java/com/springboot/aiproject/apigenerator/ApiGeneratorRequest.java
+++
@@ -1,8 +0,0 @@
-package com.springboot.aiproject.apigenerator;
-
-import java.util.List;
-
-public record ApiGeneratorRequest(
-        String entityName,      // e.g. "Employee"
-        List<String> fields     // e.g. ["id:Long", "name:String", "salary:Double"]
-) {}
